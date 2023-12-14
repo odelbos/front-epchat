befbe1449c9060c315901736150b115f0a298a77
odelbos <od@phibox.com>
2023-12-14 19:23:11 +0100

    add api service

---
+++ src/services/api_service.js
@@ -0,0 +1,32 @@
+import Config from '../config'
+
+export default class ApiService {
+
+  static prepare_headers() {
+    return {
+      'Content-type': 'application/json;charset=UTF-8',
+      // 'Authorization': 's7cr7t',
+    }
+  }
+
+  static async createChannel(data) {
+    let url = Config.api.url + 'channels/create'
+    let response = await fetch(url, {
+      method: 'POST',
+      headers: ApiService.prepare_headers(),
+      body: JSON.stringify(data)
+    })
+    return ApiService.#buildResponse(response)
+  }
+
+  // -----
+
+  static async #buildResponse(response) {
+    const contentType = response.headers.get('content-type')
+    if (contentType && contentType.indexOf('application/json') === -1) {
+      return [response.status, await response.text(), response]
+    }
+    return [response.status, await response.json(), response]
+  }
+}
+
