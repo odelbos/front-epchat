80d2fa5c0add1a2e33068fde22e807743b069b16
odelbos <od@phibox.com>
2023-12-14 19:20:12 +0100

    add application config

---
+++ src/SAMPLE.config.js
@@ -0,0 +1,12 @@
+const config = {
+  api: {
+    url: 'http://localhost:4000/api/v1/',
+  },
+  web: {
+    url: 'http://localhost:5174/',
+    sock: 'ws://localhost:4000/ws',
+  }
+}
+
+export default config
+
