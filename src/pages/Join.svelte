af1008837590d6433ed566cb285a166fbc13431b
odelbos <od@phibox.com>
2023-12-19 11:43:43 +0100

    add join channel page

---
+++ src/pages/Join.svelte
@@ -0,0 +1,86 @@
+<script>
+import { getContext } from 'svelte'
+import LocalStorageService from '../services/local_storage_service'
+import ApiService from '../services/api_service'
+import { channel } from '../stores/data'
+
+let router = getContext('ROUTER')
+export let request
+
+let channel_id = request.params.channel_id
+
+let user = null
+if (LocalStorageService.hasUser()) {
+  user = LocalStorageService.getUser()
+}
+
+let form = {
+  nickname: '',
+}
+if (user !== null) form.nickname = user.nickname
+
+let gui = {
+  btnValid: {
+    isLoading: false,
+    isDisabled: false,
+  },
+  nickname: {
+    invalid: false,
+    msg: '',
+  },
+}
+
+function validForm() {
+  if (form.nickname === null || form.nickname.trim().length < 2) {
+    gui.nickname.msg = 'You must provide a nickname of more than 1 character'
+    gui.nickname.invalid = true
+    return false
+  }
+  gui.nickname.invalid = false
+  return true
+}
+
+async function onClickValid() {
+  if (! validForm()) return
+  gui.btnValid.isLoading = true
+  gui.btnValid.isDisabled = true
+
+  let data = {
+    nickname: form.nickname,
+    channel_id: channel_id,
+  }
+  if (user !== null) data.user_id = user.id
+
+  let status, result
+  [status, result] = await ApiService.joinChannel(data)
+  if (status === 200) {
+    console.log(result)
+    LocalStorageService.setUser(result.user)
+    $channel = result.channel
+    router.navigate('room')
+  }
+  else {
+    // TODO: Properly manage the error
+    console.log(result)
+    throw 'API error'
+  }
+}
+</script>
+
+
+<div class="container mt-40">
+  <label for="nickname-field" class="form-label">Nickname <sup class="required">*</sup></label>
+  <input id="nickname-field" type="text" class="form-control" class:is-invalid={gui.nickname.invalid} bind:value={form.nickname}/>
+  <div class="invalid-msg">{gui.nickname.msg}</div>
+</div>
+
+<div class="container mt-40 text-center">
+  <button class="btn btn-primary" type="button" on:click={onClickValid} disabled={form.nickname === '' || gui.btnValid.isDisabled}>
+    {#if gui.btnValid.isLoading}
+      <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+      <span class="visually-hidden">Loading...</span>
+    {:else}
+      Join Chat Room
+    {/if}
+  </button>
+</div>
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import DefaultLayout from './layouts/DefaultLayout.svelte'
 import Home from './pages/Home.svelte'
 import Room from './pages/Room.svelte'
+import Join from './pages/Join.svelte'
 
 // -------------------------------------------------
 // Routes settings
@@ -15,6 +16,11 @@ const routes = [
     path: '/room',
     layout: DefaultLayout,
     component: Room,                        // Implicit route name : room
+  },
+  {
+    path: '/join/:channel_id',
+    layout: DefaultLayout,
+    component: Join,                        // Implicit route name : join
   },
 ]
 
