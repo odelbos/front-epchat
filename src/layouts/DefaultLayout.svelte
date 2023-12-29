580335cc94790621d4d554383b8f88f9aa1dd70e
odelbos <od@phibox.com>
2023-12-29 14:06:18 +0100

    specific header for room page

--- src/layouts/DefaultLayout.svelte
+++ src/layouts/DefaultLayout.svelte
@@ -1,6 +1,6 @@
 <script>
 import Config from '../config'
-import { subscribe } from '../stores/bus'
+import { emit, subscribe } from '../stores/bus'
 
 let headerTitle = 'EpChat'
 let channelId = null
@@ -11,23 +11,46 @@ function onClickRoomLink(_e) {
 }
 
 subscribe('layout', (_topic, data) => {
-  if (data.event === 'channel') {
-    headerTitle = 'Room: ' + data.channel_id.substring(0, 8)
+  if (data.event === 'room_header') {
     channelId = data.channel_id
+    headerTitle = 'Room: ' + data.channel_id.substring(0, 8)
   }
 })
+
+// -----
+
+async function onClickInvit() {
+  emit('layout', {event: 'click_invit', channel_id: channelId})
+}
+
+async function onClickClose() {
+  emit('layout', {event: 'click_close', channel_id: channelId})
+}
 </script>
 
 
 <div class="wrapper">
-  <header>
-    <!-- <p>EpChat</p> -->
-    {#if channelId !== null}
-      <p class="clip" on:click={onClickRoomLink}>{headerTitle}</p>
-    {:else}
+  {#if channelId !== null}
+    <header>
+      <div class="text-left pl-15">
+        <button class="btn btn-action btn-small" type="button" on:click={onClickInvit}>Invit. Link</button>
+      </div>
+      <div>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+        <p class="clip" on:click={onClickRoomLink}>{headerTitle}</p>
+      </div>
+      <div class="text-right pr-15">
+        <button class="btn btn-danger btn-small" type="button" on:click={onClickClose}>Close</button>
+      </div>
+    </header>
+  {:else}
+    <header>
+      <div></div>
       <p>{headerTitle}</p>
-    {/if}
-  </header>
+      <div></div>
+    </header>
+  {/if}
   <main>
     <slot/>
   </main>
@@ -40,10 +63,14 @@ subscribe('layout', (_topic, data) => {
 <style lang="sass">
 .wrapper
   display: grid
-  grid-template-rows: 35px auto 35px
+  grid-template-rows: 45px auto 35px
   min-height: 100vh
 
   header
+    display: grid
+    grid-template-columns: repeat(3, 1fr)
+    padding-top: 5px
+    padding-bottom: 5px
     text-align: center
     background: var(--surface1-color)
 
